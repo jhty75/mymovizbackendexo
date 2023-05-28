4d663d13a4cb8401e1f0600301f1102723a162b6
jhuguespam@gmail.com <jhuguespam@gmail.com>
2023-05-28 14:50:20 +0200

    mymovizbackendexo

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const fetch= require('node-fetch');
+const api_key = process.env.OWM_API_KEY;
+
+router.get('/movies', (req,res)=>{
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
+.then(res => res.json())
+.then(data => {
+    const movies = data.results
+    console.log(movies);
+    res.json({movies})
+    // res.json({movies:data.results});
+});
+});
+
+module.exports = router;
